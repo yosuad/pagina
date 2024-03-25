735fafda49315654f41056cdb1a9450d608b81d1
yosuad <yosuad@hotmail.com>
2024-03-25 18:11:27 -0500

    proyecto_finalizado_pagina_developers

--- pagina_developers/assets/js/menu-mobile.js
+++ pagina_developers/assets/js/menu-mobile.js
@@ -3,7 +3,8 @@ document.addEventListener("DOMContentLoaded", (Event) => {
   let mobile_btn = document.querySelector(".navbar__mobile-btn");
   let mobile_menu = document.querySelector(".menu-mobile");
 
-  mobile_btn.addEventListener("click", () => {
+  //FUNCION MOSTRAR Y OCULTAR MENU
+  const showHiddenMenu = () => {
     let show = document.querySelector(".menu-mobile--show");
 
     if (show) {
@@ -11,5 +12,38 @@ document.addEventListener("DOMContentLoaded", (Event) => {
     } else {
       mobile_menu.classList.add("menu-mobile--show");
     }
+  };
+
+  // AL DAR CLIC AL BOTON DEL MENU MOSTRAR EL MENU DE NAVEGACION
+  mobile_btn.addEventListener("click", showHiddenMenu);
+
+  //AL REDIMENSIONAR LA PANTALLA POR DEBAJO DE 1000PX OCULTAR EL MENU
+  window.addEventListener("resize", () => {
+    let window_width = parseInt(document.body.clientWidth);
+    if (window_width > 1000) {
+      mobile_menu.classList.remove("menu-mobile--show");
+    }
+  });
+
+  //CERRAR EL MENU CON EL BOTON X
+  let btn_close = document.querySelector(".menu-mobile__close");
+
+  btn_close.addEventListener("click", showHiddenMenu);
+
+  //DESPLEGAR SUBMENUS
+  let menu_item = document.querySelectorAll(".menu-mobile__item");
+
+  menu_item.forEach((item) => {
+    item.addEventListener("click", (event) => {
+      let submenu = item.lastElementChild;
+
+      if (submenu.className === "menu-mobile__submenu-mobile") {
+        if (submenu.style.display === "block") {
+          submenu.style.display = "none";
+        } else {
+          submenu.style.display = "block";
+        }
+      }
+    });
   });
 });
