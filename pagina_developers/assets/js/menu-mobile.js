24d8f30a1dae390e4c5543e38e649bffe19dbf6f
yosuad <yosuad@hotmail.com>
2024-03-25 11:23:54 -0500

    style_movil

---
+++ pagina_developers/assets/js/menu-mobile.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", (Event) => {
+  // SELECIONAR MIS DOS ELEMENTOS PRINCIPALES
+  let mobile_btn = document.querySelector(".navbar__mobile-btn");
+  let mobile_menu = document.querySelector(".menu-mobile");
+
+  mobile_btn.addEventListener("click", () => {
+    let show = document.querySelector(".menu-mobile--show");
+
+    if (show) {
+      mobile_menu.classList.remove("menu-mobile--show");
+    } else {
+      mobile_menu.classList.add("menu-mobile--show");
+    }
+  });
+});
